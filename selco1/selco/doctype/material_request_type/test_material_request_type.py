65645d70a4e2dad3558a14bcd53b29f846745570
Basawaraj <basawaraj@selco-india.com>
2017-01-03 23:41:49 +0530

    added doctypes MR item print,MR type,SR details issue

---
+++ selco1/selco/doctype/material_request_type/test_material_request_type.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015, Kishor and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+# test_records = frappe.get_test_records('Material Request Type')
+
+class TestMaterialRequestType(unittest.TestCase):
+	pass
